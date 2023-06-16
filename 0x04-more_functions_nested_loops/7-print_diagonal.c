720d52d6c5ee3893a9cc07a969a730cb133892bc
birukshegute <birukshegute@gmail.com>
2023-06-16 05:10:21 -0700

    simpler 7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,12 @@ void print_diagonal(int n)
 		_putchar('\n');
 	} else
 	{
-		int i, j;
+		int i;
 
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar('\\');
 		}
+	_putchar('\n');
 	}
 }
