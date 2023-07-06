cde6e5d20df6e5dda32c22c254082709b4f37968
birukshegute <birukshegute@gmail.com>
2023-07-06 01:00:23 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to unsigned int
+ * @b the binary that is about to be converted
+ *
+ * Reeturn - the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	
