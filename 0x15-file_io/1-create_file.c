1e92d8c7ff50d970d622ad573812f66a592f6c56
birukshegute <birukshegute@gmail.com>
2023-07-10 13:13:22 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,33 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
 
-int create_file(const char *filename, char *text_content){
+/**
+ * create_file - creates a file and puts text content in it
+ *
+ * @filename: name for the file to be created.
+ * @text_content: text content to be inseted to the file.
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
