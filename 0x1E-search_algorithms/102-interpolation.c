da291061c421efaff9ed76a80e5b89bae434ac4d
birukshegute <birukshegute@gmail.com>
2024-02-07 13:25:43 -0800

    task 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -24,6 +24,7 @@ int interpolation_search(int *array, size_t size, int value)
 		if (pos > size)
 		{
 			printf("Value checked array[%lu] is out of range\n", size);
+			return (-1);
 		}
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
